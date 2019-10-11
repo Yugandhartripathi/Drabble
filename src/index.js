810082d06cf110a77c20aa060fd2a2a8bbc55dc3
Yugandhartripathi <tripathi.yugandhar@gmail.com>
2019-10-11 19:10:07 +0530

    Add canvas

--- src/index.js
+++ src/index.js
@@ -1,12 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import CanvasDraw from 'react-canvas-draw';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+class Draw extends React.Component{
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.register();
+    render() { 
+        return(
+        <div class="Container">
+            <h1>Drabble</h1>
+            <div style={{border: "10px solid black", width: "400px"}}>
+                <CanvasDraw brushRadius={"5"} lazyRadius={"2"} brushColor={"#000"}/>
+            </div>
+        </div>
+        );
+    }
+}
+ReactDOM.render(<Draw />, document.getElementById("root"));
